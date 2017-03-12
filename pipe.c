e72818f95e9ba3215eac82b8aab590aff3bde86c
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-03-12 21:00:32 +1300

    add timing info

--- pipe.c
+++ pipe.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 /* WARNING. No error handling */
 char *read_entire_file(char *name)
@@ -29,23 +30,34 @@ int main(void)
 	char *license = read_entire_file("LICENSE");
 	long fsize = strlen(license); /* Yes. I know */
 	char *buffer = malloc(fsize + 1);
+	long runs = 1000000; /* 1 million dollars */
+
+	clock_t start, end;
+
+	start = clock();
 
     pipe(pfds);
 
     if (!fork())
 		{
         printf(" CHILD: writing to the pipe\n");
-        write(pfds[1], license, fsize + 1);
+		for (long i = 0; i < runs; i++)
+        	write(pfds[1], license, fsize + 1);
         printf(" CHILD: exiting\n");
         exit(0);
 		}
 	else
 		{
         printf("PARENT: reading from pipe\n");
-        read(pfds[0], buffer, fsize + 1);
-        printf("PARENT: read \"%s\"\n", buffer);
+		for (long i = 0; i < runs; i++)
+        	read(pfds[0], buffer, fsize + 1);
+        printf("PARENT: read\n");
         wait(NULL);
     	}
+
+	end = clock();
+	fprintf(stderr, "Runs     | Time taken\n");
+	fprintf(stderr, "%8d   %f\n", runs, (end - start) / (double)CLOCKS_PER_SEC);
 
     return 0;
 	}
